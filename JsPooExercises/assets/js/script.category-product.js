307006db43e06610d6f7ef21f06d235014c8793f
Maria Rita <125027496+MariaRita011@users.noreply.github.com>
2023-08-18 10:04:26 -0300

    feat: create class of category

---
+++ JsPooExercises/assets/js/script.category-product.js
@@ -0,0 +1,10 @@
+//Cada categoria possui vários produtos 
+//Cada produto é pertencente a uma categoria
+
+class Category {
+    constructor(id, name) {
+        this.id = id;
+        this.nam = name;
+    }
+}
+
